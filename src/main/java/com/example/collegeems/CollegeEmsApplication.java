a3a0c3f849bdee6ec367164a67026bcf78f55f73
Along <yuanlong802@gmail.com>
2025-01-14 23:29:36 +0800

    fastcode

---
+++ src/main/java/com/example/collegeems/CollegeEmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.collegeems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeEmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeEmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/collegeems/CollegeEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collegeems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeEmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
